ef90d279e5b28ed96c40fd8eb45d59acb1191f5a
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-09 21:58:15 +0200

    Installed profiler and added first template and controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,8 +21,14 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: %s'
-        , $slug));
+        $comments = [
+            'I ate a normal rock once. It did not taste like bacon.',
+            'Whoooo! I\'m going on an all-asteroids diet.',
+            'I like bacon too! Buy some from my site! oamogetswemgidi.com',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
