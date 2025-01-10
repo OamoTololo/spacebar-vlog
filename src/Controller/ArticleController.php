b4239dc50d372b572a4aa2a368bbd34ae0e878e6
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-10 08:22:25 +0200

    Created a JSON API Endpoint.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,7 +29,18 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        // TODO - actually heart/un-heart the article
+
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
