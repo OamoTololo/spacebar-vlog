2b38e8f6486a0ae693f0ae0ecaf751ff88628218
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-10 15:49:22 +0200

    Used the cache service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,7 +22,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownParserInterface $markdownParser, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did not taste like bacon.',
@@ -30,24 +31,32 @@ class ArticleController extends AbstractController
         ];
 
         $articleContent = <<<EOF
-    Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
-    lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
-    labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-    **turkey** shank eu pork belly meatball non cupim.
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
     
-    Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
-    laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
-    capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-    picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
-    occaecat lorem meatball prosciutto quis strip steak.
-    
-    Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
-    mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
-    strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
-    cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-    fugiat.
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
 EOF;
-        $articleContent = $markdownParser->transformMarkdown($articleContent);
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+
+        if (!$item->isHit()) {
+            $item->set($articleContent);
+            $cache->save($item);
+        }
+
+        //$articleContent = $markdownParser->transformMarkdown($articleContent);
+        $articleContent = $item->get();
 
 
         return $this->render('article/show.html.twig', [
